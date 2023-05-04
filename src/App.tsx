36e22098309af2ae4a6f6365007a793cf5715cd6
Lucas Ribeiro <100320271+LUKASRIB15@users.noreply.github.com>
2023-05-04 16:16:13 -0300

    Desenvolvendo container onde serão guardada as listas (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header'
 import styles from "./App.module.css"
 import './global.css'
 import { Search } from './components/Search'
+import { ListTasks } from './components/ListTasks'
 
 export function App() {
   return (
@@ -10,6 +11,7 @@ export function App() {
       <Header/>
       <div className={styles.wrapper}>
         <Search/>
+        <ListTasks/>
       </div>
     </>
   )
