01629cc34593a3c06255aa2a335a014a13d6646d
Lucas Ribeiro <100320271+LUKASRIB15@users.noreply.github.com>
2023-05-05 15:56:01 -0300

    Fazendo o usuário poder remover a task e adicionando a key como um id (#16)

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,28 @@ import styles from "./App.module.css"
 import './global.css'
 import { ListTasks } from './components/ListTasks'
 import { Search } from './components/Search'
+import { objectListTasks } from './components/ListTasks'
 
 export function App() {
   const [newTask, setNewTask] = useState('')
-  const [tasks, setTasks] = useState<string[]>([])
+  const [tasks, setTasks] = useState<objectListTasks[]>([])
 
   function handleCreateNewTask(event: ChangeEvent<HTMLFormElement>){
     event.preventDefault()
     setTasks((state) =>{
-      return [...state, newTask]
+      return [...state, {id: crypto.randomUUID(), content: newTask}]
     })
   }
   // Adicionando o valor em tempo real
   function handleNewTask(event: ChangeEvent<HTMLInputElement>){
     setNewTask(event.target.value)
+  }
+
+  function handleDeleteTask(taskDeleted:string){
+    const newListTasks = tasks.filter((task)=>{
+      return task.content!=taskDeleted;
+    }) 
+    setTasks(newListTasks); 
   }
   return (
     <>
@@ -29,7 +37,10 @@ export function App() {
           onNewTask={handleNewTask} 
           valueInput={newTask}
         />
-        <ListTasks listTasks={tasks}/>
+        <ListTasks 
+          listTasks={tasks}
+          removingTask={handleDeleteTask}
+        />
       </div>
     </>
   )
