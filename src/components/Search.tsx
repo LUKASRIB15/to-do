40a4d22b27fe8a60259aaabcc460fa0f0c917574
Lucas Ribeiro <100320271+LUKASRIB15@users.noreply.github.com>
2023-05-05 10:21:00 -0300

    Criando tarefas (#14)

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,31 @@
 import { PlusCircle } from "phosphor-react"
 import styles from "./Search.module.css"
+import {useState, ChangeEvent} from "react"
 
 export function Search(){
+  const [newTask, setNewTask] = useState('')
+  const [tasks, setTasks] = useState<string[]>([])
+
+  function handleCreateNewTask(event: ChangeEvent<HTMLFormElement>){
+    event.preventDefault()
+    setTasks((state) =>{
+      return [...state, newTask]
+    })
+  }
+  // Adicionando o valor em tempo real
+  function handleNewTask(event: ChangeEvent<HTMLInputElement>){
+    setNewTask(event.target.value)
+  }
+
   return(
-    <form className={styles.formContent}>
-      <input className={styles.inputField} type="text" placeholder="Adicione uma nova tarefa"/>
+    <form onSubmit={handleCreateNewTask} className={styles.formContent}>
+      <input 
+        className={styles.inputField} 
+        type="text" 
+        placeholder="Adicione uma nova tarefa"
+        onChange={handleNewTask}
+        value={newTask}
+      />
       <button className={styles.createButton} type="submit">
         Criar
         <PlusCircle size={20}/>
